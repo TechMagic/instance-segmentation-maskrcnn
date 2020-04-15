32cbf80831c19d3a273cf3f7fea1daec2d7a1abf
WimFlorijn <wimflorijn>
2020-04-15 09:45:40 +0200

    Revert to old version

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -7,18 +7,25 @@ Licensed under the MIT License (see LICENSE for details)
 Written by Waleed Abdulla
 """
 
+import os
+import sys
 import random
 import itertools
 import colorsys
+
 import numpy as np
 from skimage.measure import find_contours
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.lines as lines
+from matplotlib import patches,  lines
 from matplotlib.patches import Polygon
 import IPython.display
 
-import vector.utils.maskrcnn.utils
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
 
 
 ############################################################
@@ -33,11 +40,11 @@ def display_images(images, titles=None, cols=4, cmap=None, norm=None,
     cols: number of images per row
     cmap: Optional. Color map to use. For example, "Blues".
     norm: Optional. A Normalize instance to map values to colors.
-    interpolation: Optional. Image interporlation to use for display.
+    interpolation: Optional. Image interpolation to use for display.
     """
     titles = titles if titles is not None else [""] * len(images)
     rows = len(images) // cols + 1
-    plt.figure(figsize=(14, 5 * rows // cols)) # was (14, 14 * rows // cols))
+    plt.figure(figsize=(14, 14 * rows // cols))
     i = 1
     for image, title in zip(images, titles):
         plt.subplot(rows, cols, i)
@@ -75,14 +82,20 @@ def apply_mask(image, mask, color, alpha=0.5):
 
 def display_instances(image, boxes, masks, class_ids, class_names,
                       scores=None, title="",
-                      figsize=(16, 16), ax=None):
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None):
     """
     boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
     masks: [height, width, num_instances]
     class_ids: [num_instances]
     class_names: list of class names of the dataset
     scores: (optional) confidence scores for each box
-    figsize: (optional) the size of the image.
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
     """
     # Number of instances
     N = boxes.shape[0]
@@ -91,11 +104,14 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     else:
         assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
 
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
     if not ax:
         _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = True
 
     # Generate random colors
-    colors = random_colors(N)
+    colors = colors or random_colors(N)
 
     # Show area outside image boundaries.
     height, width = image.shape[:2]
@@ -113,23 +129,27 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             # Skip this instance. Has no bbox. Likely lost in image cropping.
             continue
         y1, x1, y2, x2 = boxes[i]
-        p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
-                              alpha=0.7, linestyle="dashed",
-                              edgecolor=color, facecolor='none')
-        ax.add_patch(p)
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
 
         # Label
-        class_id = class_ids[i]
-        score = scores[i] if scores is not None else None
-        label = class_names[class_id]
-        x = random.randint(x1, (x1 + x2) // 2)
-        caption = "{} {:.3f}".format(label, score) if score else label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
         ax.text(x1, y1 + 8, caption,
                 color='w', size=11, backgroundcolor="none")
 
         # Mask
         mask = masks[:, :, i]
-        masked_image = apply_mask(masked_image, mask, color)
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
 
         # Mask Polygon
         # Pad to ensure proper polygons for masks that touch image edges.
@@ -143,8 +163,47 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    plt.show()
-    
+    if auto_show:
+        plt.show()
+
+
+def display_differences(image,
+                        gt_box, gt_class_id, gt_mask,
+                        pred_box, pred_class_id, pred_score, pred_mask,
+                        class_names, title="", ax=None,
+                        show_mask=True, show_box=True,
+                        iou_threshold=0.5, score_threshold=0.5):
+    """Display ground truth and prediction instances on the same image."""
+    # Match predictions to ground truth
+    gt_match, pred_match, overlaps = utils.compute_matches(
+        gt_box, gt_class_id, gt_mask,
+        pred_box, pred_class_id, pred_score, pred_mask,
+        iou_threshold=iou_threshold, score_threshold=score_threshold)
+    # Ground truth = green. Predictions = red
+    colors = [(0, 1, 0, .8)] * len(gt_match)\
+           + [(1, 0, 0, 1)] * len(pred_match)
+    # Concatenate GT and predictions
+    class_ids = np.concatenate([gt_class_id, pred_class_id])
+    scores = np.concatenate([np.zeros([len(gt_match)]), pred_score])
+    boxes = np.concatenate([gt_box, pred_box])
+    masks = np.concatenate([gt_mask, pred_mask], axis=-1)
+    # Captions per instance show score/IoU
+    captions = ["" for m in gt_match] + ["{:.2f} / {:.2f}".format(
+        pred_score[i],
+        (overlaps[i, int(pred_match[i])]
+            if pred_match[i] > -1 else overlaps[i].max()))
+            for i in range(len(pred_match))]
+    # Set title if not provided
+    title = title or "Ground Truth and Detections\n GT=green, pred=red, captions: score/IoU"
+    # Display
+    display_instances(
+        image,
+        boxes, masks, class_ids,
+        class_names, scores, ax=ax,
+        show_bbox=show_box, show_mask=show_mask,
+        colors=colors, captions=captions,
+        title=title)
+
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
     """
@@ -264,7 +323,7 @@ def plot_overlaps(gt_class_ids, pred_class_ids, pred_scores,
     gt_class_ids: [N] int. Ground truth class IDs
     pred_class_id: [N] int. Predicted class IDs
     pred_scores: [N] float. The probability scores of predicted classes
-    overlaps: [pred_boxes, gt_boxes] IoU overlaps of predictins and GT boxes.
+    overlaps: [pred_boxes, gt_boxes] IoU overlaps of predictions and GT boxes.
     class_names: list of all class names in the dataset
     threshold: Float. The prediction probability required to predict a class
     """
@@ -300,7 +359,7 @@ def plot_overlaps(gt_class_ids, pred_class_ids, pred_scores,
 def draw_boxes(image, boxes=None, refined_boxes=None,
                masks=None, captions=None, visibilities=None,
                title="", ax=None):
-    """Draw bounding boxes and segmentation masks with differnt
+    """Draw bounding boxes and segmentation masks with different
     customizations.
 
     boxes: [N, (y1, x1, y2, x2, class_id)] in image coordinates.
@@ -309,7 +368,7 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
     masks: [N, height, width]
     captions: List of N titles to display on each box
     visibilities: (optional) List of values of 0, 1, or 2. Determine how
-        prominant each bounding box should be.
+        prominent each bounding box should be.
     title: An optional title to show over the image
     ax: (optional) Matplotlib axis to draw on.
     """
@@ -376,7 +435,6 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
             # If there are refined boxes, display captions on them
             if refined_boxes is not None:
                 y1, x1, y2, x2 = ry1, rx1, ry2, rx2
-            x = random.randint(x1, (x1 + x2) // 2)
             ax.text(x1, y1, caption, size=11, verticalalignment='top',
                     color='w', backgroundcolor="none",
                     bbox={'facecolor': color, 'alpha': 0.5,
@@ -385,7 +443,7 @@ def draw_boxes(image, boxes=None, refined_boxes=None,
         # Masks
         if masks is not None:
             mask = masks[:, :, i]
-            masked_image = apply_mask(masked_image, mask, color,alpha=0.3)
+            masked_image = apply_mask(masked_image, mask, color)
             # Mask Polygon
             # Pad to ensure proper polygons for masks that touch image edges.
             padded_mask = np.zeros(
@@ -440,166 +498,3 @@ def display_weight_stats(model):
                 "{:+9.4f}".format(w.std()),
             ])
     display_table(table)
-
-
-def display_gt_and_pred(image, gt_boxes, gt_masks, gt_class_ids, gt_class_names, pred_boxes, pred_masks, pred_class_ids,
-                       pred_class_names, pred_scores=None, figsize=(16, 8), title0=None, savename=None):
-    """
-    Lime. similar to displayInstances(), but displays the raw img, ground truth and prediction in one go.
-
-    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
-    masks: [height, width, num_instances]
-    class_ids: [num_instances]
-    class_names: list of class names of the dataset
-    scores: (optional) confidence scores for each box (for prediction only)
-    figsize: (optional) the size of the image.
-    title0: option to custom-set title of first subplot
-    savename: if not None: saves image
-    """
-
-    _, axarray = plt.subplots(1,3, figsize=figsize)
-    plt.subplots_adjust(wspace=0.0,left=0, right=1) # decrease the whitespace
-
-    for ax in axarray:
-        # Show area outside image boundaries.
-        height, width = image.shape[:2]
-        ax.set_ylim(height + 10, -10)
-        ax.set_xlim(-10, width + 10)
-        ax.axis('off')
-
-    # show input image
-    fsize=16
-    axarray[0].imshow(image)
-    if not title0:
-        axarray[0].set_title('input',fontsize=fsize)
-    else:
-        axarray[0].set_title(title0,fontsize=fsize)
-
-    # show ground truth
-    masked_image=create_masked_image(image, axarray[1], gt_boxes,gt_masks, gt_class_ids, gt_class_names)
-    axarray[1].imshow(masked_image.astype(np.uint8))
-    axarray[1].set_title('ground truth',fontsize=fsize)
-
-    # show prediction
-    masked_image=create_masked_image(image, axarray[2], pred_boxes,pred_masks, pred_class_ids, pred_class_names, pred_scores)
-    axarray[2].imshow(masked_image.astype(np.uint8))
-    axarray[2].set_title('prediction',fontsize=fsize)
-    
-    if savename is not None:
-        plt.savefig(savename,dpi=300)
-
-    plt.show()
-
-
-def display_pred(image, pred_boxes, pred_masks, pred_class_ids,
-                       pred_class_names, pred_scores=None, figsize=(8, 16), title0=None, savename=None):
-    """
-    Lime. similar to displayInstances(), but displays the raw img, ground truth and prediction in one go.
-
-    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
-    masks: [height, width, num_instances]
-    class_ids: [num_instances]
-    class_names: list of class names of the dataset
-    scores: (optional) confidence scores for each box (for prediction only)
-    figsize: (optional) the size of the image.
-    title0: option to custom-set title of first subplot
-    savename: if not None: saves image
-    """
-    
-    _, ax = plt.subplots(1,1, figsize=figsize)
-    plt.subplots_adjust(wspace=0.0,left=0, right=1) # decrease the whitespace
-
-    
-    # Show area outside image boundaries.
-    height, width = image.shape[:2]
-    ax.set_ylim(height + 10, -10)
-    ax.set_xlim(-10, width + 10)
-    ax.axis('off')
-
-    # show prediction
-    masked_image=create_masked_image(image, ax, pred_boxes,pred_masks, pred_class_ids, pred_class_names, pred_scores)
-    ax.imshow(masked_image.astype(np.uint8))
-    ax.set_title('prediction',fontsize=16)
-    
-    if savename is not None:
-        plt.savefig(savename,dpi=300)
-
-    plt.show()
-
-
-def create_masked_image(image,ax,boxes, masks, class_ids, class_names,scores=None):
-    """
-    Lime.
-    extracted & refactored from display_instances(), for greater flexibility.
-
-    ax: matplotlib axis array (e.g. _,ax=plt.subplots(1,1)) (or a selected subplot)
-    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
-    masks: [height, width, num_instances]
-    class_ids: [num_instances]
-    class_names: list of class names of the dataset
-    scores: (optional) confidence scores for each box
-    """
-
-    # Number of instances
-    N = boxes.shape[0]
-    if not N:
-        print("*** No instances to display *** ")
-    else:
-        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
-
-    # Generate random colors
-    colors = random_colors(N)
-
-    masked_image = image.astype(np.uint32).copy()
-    for i in range(N):
-        color = colors[i]
-
-        # Bounding box
-        if not np.any(boxes[i]):
-            # Skip this instance. Has no bbox. Likely lost in image cropping.
-            continue
-        y1, x1, y2, x2 = boxes[i]
-        p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
-                              alpha=0.7, linestyle="dashed",
-                              edgecolor=color, facecolor='none')
-        ax.add_patch(p)
-
-        # Label
-        class_id = class_ids[i]
-        score = scores[i] if scores is not None else None
-        label = class_names[class_id]
-        x = random.randint(x1, (x1 + x2) // 2)
-        caption = "{} {:.3f}".format(label, score) if score else label
-        ax.text(x1, y1 + 8, caption,
-                color='w', size=14, backgroundcolor="none")
-
-        # Mask
-        mask = masks[:, :, i]
-        masked_image = apply_mask(masked_image, mask, color,alpha=0.3)
-
-        # Mask Polygon
-        # Pad to ensure proper polygons for masks that touch image edges.
-        padded_mask = np.zeros(
-            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
-        padded_mask[1:-1, 1:-1] = mask
-        contours = find_contours(padded_mask, 0.5)
-        for verts in contours:
-            # Subtract the padding and flip (y, x) to (x, y)
-            verts = np.fliplr(verts) - 1
-            p = Polygon(verts, facecolor="none", edgecolor=color)
-            ax.add_patch(p)
-
-    return masked_image
-
-
-# from train_shapes notebook.
-def get_ax(rows=1, cols=1, size=8):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-
-    Change the default size attribute to control the size
-    of rendered images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
-    return ax
