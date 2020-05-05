c62a1ee9551d3a167fcb4100412b605275324a2f
WimFlorijn <wimflorijn>
2020-05-05 19:01:47 +0200

    Increase resolution

--- train/create_train_data.py
+++ train/create_train_data.py
@@ -11,13 +11,11 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from skimage import transform
 
-HULL_TH = 200
-TARGET_SHAPE = (1280, 768)
-
+TARGET_SHAPE = (2048, 1280)
 IN_DIR, OUT_DIR = 'C:/Users/FlorijnWim/Downloads/verwerkt', '../train_data'
 
 
-def generate_box_mask(box, mask_shape, image_gs):
+def generate_box_mask(box, mask_shape):
     rct = ((box[0][0], box[0][1]), (box[1][0], box[1][1]), box[2])
     corner_points = cv2.boxPoints(rct).astype(np.int32)
     mask = np.zeros(mask_shape, dtype=np.uint8)
@@ -27,36 +25,10 @@ def generate_box_mask(box, mask_shape, image_gs):
         .astype(np.uint8)
     mask = np.nonzero(mask)
 
-    zeros = np.zeros(image_gs.shape)
-    zeros[mask] = image_gs[mask] < HULL_TH
-    mask = np.nonzero(zeros)
-
-    image = np.zeros(image_gs.shape)
-    if len(mask[0]):
-        mask_points = np.hstack((mask[1][:, None], mask[0][:, None]))
-        hull = cv2.convexHull(mask_points)
-        cv2.drawContours(image, [hull], -1, 1, thickness=-1)
-
-    mask = np.nonzero(image)
-
     return mask
 
 
-def generate_building_mask(building, mask_shape, image_gs):
-    mask = np.zeros(mask_shape)
-    if len(building):
-        building = np.array([np.asarray(building)])
-        cv2.fillPoly(mask, [building], 1)
-
-    mask = transform.resize(mask, output_shape=image_gs.shape, mode='edge', order=0, preserve_range=True) \
-        .astype(np.uint8)
-
-    mask = np.nonzero(mask)
-
-    return mask
-
-
-def generate_measurement_masks_from_json(obs, image_shape, image_gs):
+def generate_measurement_masks_from_json(obs, image_shape):
     dct = obs['text']
 
     measurement_dict = {}
@@ -68,41 +40,29 @@ def generate_measurement_masks_from_json(obs, image_shape, image_gs):
     masks = []
     for txt in measurement_dict:
         box = measurement_dict[txt]['box']
-        masks.append(generate_box_mask(box, image_shape, image_gs))
+        masks.append(generate_box_mask(box, image_shape))
 
     return masks
 
 
-def generate_parcel_number_masks_from_json(obs, image_shape, image_gs):
+def generate_parcel_number_masks_from_json(obs, image_shape):
     dct = obs['text']
 
     parcel_dict = {}
     for key in dct:
         item = dct[key]
-        if item['type'] == 'parcel':
+        if item['type'] == 'parcel' and 'value' in item.keys():
             parcel_dict[key] = item
 
     masks = []
     for txt in parcel_dict:
         box = parcel_dict[txt]['box']
-        masks.append(generate_box_mask(box, image_shape, image_gs))
+        masks.append(generate_box_mask(box, image_shape))
 
     return masks
 
 
-def generate_building_from_json(obs, image_shape, image_gs):
-    points_dict, buildings = obs['points'], obs['buildings']
-
-    buildings_list = []
-    for building, points in buildings.items():
-        buildings_list.append(list(map(lambda x: list(map(int, points_dict[x]['position'])), points['points'])))
-
-    masks = list(map(lambda x: generate_building_mask(x, image_shape, image_gs), buildings_list))
-
-    return masks
-
-
-def get_masked(img, measurement_masks, parcel_number_masks, building_masks, visualize=False):
+def get_masked(img, measurement_masks, parcel_number_masks, visualize=False):
     alpha = 0.4
     masked = np.zeros(img.shape)
     mask_img = img.copy() if visualize else None
@@ -126,39 +86,35 @@ def get_masked(img, measurement_masks, parcel_number_masks, building_masks, visu
             if visualize:
                 mask_img[mask] = mask_img[mask] * [2., 1., 1.] * alpha + (1 - alpha)
 
-    non_overlapping_building_masks = []
-    if building_masks is not None:
-        for mask in building_masks:
-            non_overlapping_idx = np.where(masked[mask] == 0)[0]
-            if len(non_overlapping_idx):
-                mask = (mask[0][non_overlapping_idx], mask[1][non_overlapping_idx])
-            non_overlapping_building_masks.append(mask)
-
-            if visualize:
-                mask_img[mask] = mask_img[mask] * [1., 1., 2.] * alpha + (1 - alpha)
-
     if visualize:
         plt.figure(figsize=(4, 4))
         plt.imshow(mask_img)
         plt.show()
 
-    return measurement_masks, non_overlapping_parcel_number_masks, non_overlapping_building_masks
+    return measurement_masks, non_overlapping_parcel_number_masks
 
 
-def read_zip(zip_name, measurement_masks=True, parcel_number_masks=True, building_masks=True):
+def get_parcel_numbers(json_data):
+    parcel_numbers = set()
+    for text_id, data in json_data['text'].items():
+        if data['type'] == 'parcel' and 'value' in data.keys():
+            parcel_numbers.add(data['value'])
+
+    return parcel_numbers
+
+
+def read_zip(zip_name, measurement_masks=True, parcel_number_masks=True):
     with zipfile.ZipFile(zip_name, 'r') as archive:
         prefix, postfix = 'observations/snapshots/latest/', '.latest.json'
         sketch_files = list(filter(lambda x: x.startswith(prefix) and x.endswith(postfix), archive.namelist()))
         for i, sketch_file in enumerate(sketch_files):
             sketch_name = sketch_file[len(prefix):-len(postfix)]
-
             logging.info('Processing sketch: {index}: {name}.'.format(index=i, name=sketch_name))
 
-            attachment_prefix, img_extension = 'observations/attachments/front/' + sketch_name, '.JPG'
-            image_files = list(filter(lambda x: x.startswith(attachment_prefix) and x.endswith(img_extension),
-                                      archive.namelist()))
-            if len(image_files):
-                file = archive.read(image_files[0])
+            img_pf, img_extension = 'observations/attachments/front/' + sketch_name, '.JPG'
+            img_files = list(filter(lambda x: x.startswith(img_pf) and x.endswith(img_extension), archive.namelist()))
+            if len(img_files):
+                file = archive.read(img_files[0])
                 image = cv2.imdecode(np.frombuffer(file, np.uint8), 1)
                 image_shape = image.shape[:2]
 
@@ -168,15 +124,31 @@ def read_zip(zip_name, measurement_masks=True, parcel_number_masks=True, buildin
 
                 with archive.open(sketch_file, 'r') as fh:
                     json_data = json.loads(fh.read())
-                image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+                if parcel_number_masks:
+                    with archive.open('sketches/' + sketch_name + '/sketch.json', 'r') as fh:
+                        meta_data = json.loads(fh.read())
+
+                    parcel_numbers_vectorized = get_parcel_numbers(json_data)
+                    parcel_numbers = set(map(lambda x: str(x['parcel']), meta_data['meta']['parcels']))
+                    if not parcel_numbers == parcel_numbers_vectorized:
+                        logging.info('Skipping {name}. Not all parcel numbers are present.'.format(name=sketch_name))
+                        continue
+
+                    p_masks = generate_parcel_number_masks_from_json(json_data, image_shape)
+                else:
+                    p_masks = None
 
                 if measurement_masks:
-                    m_masks = generate_measurement_masks_from_json(json_data, image_shape, image_gs)
+                    m_masks = generate_measurement_masks_from_json(json_data, image_shape)
 
                     if not len(m_masks):
+                        logging.info('Skipping {name}. No measurements are present.'.format(name=sketch_name))
                         continue
                 else:
                     m_masks = None
+
+                m_masks, p_masks = get_masked(image, m_masks, p_masks, visualize=False)
 
                 base_dir = os.path.join(OUT_DIR, sketch_name)
                 if os.path.exists(base_dir):
@@ -187,33 +159,15 @@ def read_zip(zip_name, measurement_masks=True, parcel_number_masks=True, buildin
                     measurement_output_path = os.path.join(base_dir, 'measurement_masks')
                     Path(measurement_output_path).mkdir(parents=True, exist_ok=True)
 
-                if parcel_number_masks:
-                    parcel_output_path = os.path.join(base_dir, 'parcel_number_masks')
-                    Path(parcel_output_path).mkdir(parents=True, exist_ok=True)
-                    p_masks = generate_parcel_number_masks_from_json(json_data, image_shape, image_gs)
-                else:
-                    p_masks = None
-
-                if building_masks:
-                    building_output_path = os.path.join(base_dir, 'building_masks')
-                    Path(building_output_path).mkdir(parents=True, exist_ok=True)
-                    b_masks = generate_building_from_json(json_data, image_shape, image_gs)
-                else:
-                    b_masks = None
-
-                m_masks, p_masks, b_masks = get_masked(image, m_masks, p_masks, b_masks, visualize=False)
-
-                if measurement_masks:
                     for index, mask in enumerate(m_masks):
                         np.save(os.path.join(measurement_output_path, '{i}.npy'.format(i=index)), mask)
 
                 if parcel_number_masks:
+                    parcel_output_path = os.path.join(base_dir, 'parcel_number_masks')
+                    Path(parcel_output_path).mkdir(parents=True, exist_ok=True)
+
                     for index, mask in enumerate(p_masks):
                         np.save(os.path.join(parcel_output_path, '{i}.npy'.format(i=index)), mask)
-
-                if building_masks:
-                    for index, mask in enumerate(b_masks):
-                        np.save(os.path.join(building_output_path, '{i}.npy'.format(i=index)), mask)
 
                 cv2.imwrite(os.path.join(base_dir, 'image.tif'), image)
 
@@ -224,5 +178,5 @@ if __name__ == '__main__':
     zip_file_names = os.listdir(IN_DIR)
     random.shuffle(zip_file_names)
     for j, name in enumerate(zip_file_names):
-        logging.info('Processing project: {index}: {name}.'.format(index=j, name=name))
-        read_zip(os.path.join(IN_DIR, name), building_masks=False)
+        logging.info('Processing project {index}: {name}.'.format(index=j, name=name))
+        read_zip(os.path.join(IN_DIR, name))
